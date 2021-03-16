b7e460f7052c41973a27744c5eb3d845960ddb6f
César Téllez <carodrigueztellez@ucundinamarca.edu.co>
2021-03-16 11:58:57 -0500

    Primer commit

---
+++ src/main/java/co/edu/unicundi/areavolumenfigurasg/controlador/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.unicundi.areavolumenfigurasg.controlador;
+
+/**
+ *
+ * @author cesar
+ */
+public class Principal {
+    public static void main(String[] args) {
+        
+    }
+}
