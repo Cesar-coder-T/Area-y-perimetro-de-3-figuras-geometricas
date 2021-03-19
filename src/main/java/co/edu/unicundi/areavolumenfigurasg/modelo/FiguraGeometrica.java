710cf73622857368b069671b9efb4256eaa590e1
César Téllez <carodrigueztellez@ucundinamarca.edu.co>
2021-03-19 10:49:17 -0500

    Se modifico la clase FiguraGeometrica y se creo la clase VistaUsuario

--- src/main/java/co/edu/unicundi/areavolumenfigurasg/modelo/FiguraGeometrica.java
+++ src/main/java/co/edu/unicundi/areavolumenfigurasg/modelo/FiguraGeometrica.java
@@ -9,10 +9,45 @@ package co.edu.unicundi.areavolumenfigurasg.modelo;
  *
  * @author cesar
  */
-public class FiguraGeometrica {
+public abstract class FiguraGeometrica{
     
-    double area;
-    double perimetro;
-    double volumen;
+    private double area;
+    private double perimetro;
+
+    public FiguraGeometrica(){
+        
+    }
+    
+    public abstract double calcularArea();
+    public abstract double calcularPerimetro();
+    
+    /**
+     * @return the area
+     */
+    public double getArea() {
+        return area;
+    }
+
+    /**
+     * @param area the area to set
+     */
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    /**
+     * @return the perimetro
+     */
+    public double getPerimetro() {
+        return perimetro;
+    }
+
+    /**
+     * @param perimetro the perimetro to set
+     */
+    public void setPerimetro(double perimetro) {
+        this.perimetro = perimetro;
+    }
+    
     
 }
---
+++ src/main/java/co/edu/unicundi/areavolumenfigurasg/vista/VistaUsuario.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.unicundi.areavolumenfigurasg.vista;
+
+/**
+ *
+ * @author cesar
+ */
+public class VistaUsuario {
+    
+    public void mostrarMenu(){
+        
+    }
+}
